72013c1f7594d953618739989d6934bef602bf7a
KNightFuryX <60267521+KNightFuryX@users.noreply.github.com>
2022-02-21 23:47:11 +0545

    initial commit

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-
+from .models import ToDoList, Item
 # Create your views here.
-def index(response):
-    return HttpResponse("Index page")   
+def index(response, id):
+    tdl = ToDoList.objects.get(id=id)
+    item = tdl.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br><h2>%s</h2>" %(tdl.name, str(item.text)))   
 
 def home(response):
     return HttpResponse("home page")
